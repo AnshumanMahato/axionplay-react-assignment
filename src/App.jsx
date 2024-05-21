cdb13537c272ab0f2dca6f3ae0a8d51b432aac55
AnshumanMahato <58422570+AnshumanMahato@users.noreply.github.com>
2024-05-21 16:19:35 +0530

    chore: header refactored into separate componenet

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,9 @@
-import { Flex } from '@chakra-ui/react';
-import { FaCartShopping, FaUser } from 'react-icons/fa6';
+import Header from './components/Header';
+
 function App() {
   return (
     <>
-      <Flex
-        width="100%"
-        p={['2rem', '3rem', '4rem']}
-        justify="space-between"
-        align="center"
-      >
-        <Flex
-          fontFamily="logo"
-          direction="column"
-          justify="center"
-          align="center"
-          fontSize="2rem"
-          lineHeight={1}
-        >
-          <span>Tech</span>
-          <span>Town</span>
-        </Flex>
-        <Flex
-          gap="2rem"
-          align="center"
-          justify="space-between"
-          fontSize="2.5rem"
-        >
-          <FaCartShopping />
-          <FaUser />
-        </Flex>
-      </Flex>
+      <Header />
     </>
   );
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,32 @@
+import { Flex } from '@chakra-ui/react';
+import { FaCartShopping, FaUser } from 'react-icons/fa6';
+
+function Header() {
+  return (
+    <Flex
+      as="header"
+      width="100%"
+      p={['2rem', '3rem', '4rem']}
+      justify="space-between"
+      align="center"
+    >
+      <Flex
+        fontFamily="logo"
+        direction="column"
+        justify="center"
+        align="center"
+        fontSize="2rem"
+        lineHeight={1}
+      >
+        <span>Tech</span>
+        <span>Town</span>
+      </Flex>
+      <Flex gap="2rem" align="center" justify="space-between" fontSize="2.5rem">
+        <FaCartShopping />
+        <FaUser />
+      </Flex>
+    </Flex>
+  );
+}
+
+export default Header;
