ed8ed69ab24c6641da0fe3d35f0bb138965538d3
AnshumanMahato <58422570+AnshumanMahato@users.noreply.github.com>
2024-05-25 12:01:04 +0530

    fix: adjusted base route for deployment environment

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/axionplay-assignment/',
+  base: '/axionplay-react-assignment/',
 });
 
