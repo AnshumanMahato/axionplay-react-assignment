807bc9c40f0925d5bf0f7f3e831f5de22f71d4ff
AnshumanMahato <58422570+AnshumanMahato@users.noreply.github.com>
2024-05-25 12:18:54 +0530

    fix: fixed 404 error on page refresh for gh pages deployment

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import RootLayout from './views/RootLayout';
 import Catalogue from './views/Catalogue';
 import ProductDetails from './views/ProductDetails';
 
+// Using hash router instead of browser router to avoid 404 error on page refresh on ghpages.
+// Vercel and netlify will work fine with browser router"
 const router = createHashRouter([
   {
     path: '/',
